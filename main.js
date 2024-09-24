dae26cf2f8861c82cd7af21e319f2494de261231
walaa-25 <gydrudt.65734bcvnbr@gmail.com>
2024-09-24 23:18:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let title=document.getElementById('title');
+let price=document.getElementById('price');
+let taxes=document.getElementById('taxes');
+let ads=document.getElementById('ads');
+let discount=document.getElementById('discount');
+let totle=document.getElementById('totle');
+let count=document.getElementById('count');
+let catogery=document.getElementById('catogery');
+let create=document.getElementById('create');
+function getTotle(){
+    console.log('done');
+}
